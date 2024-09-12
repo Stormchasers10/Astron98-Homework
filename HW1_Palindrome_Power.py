9d94e2a190becd8d70359d62f4408877c1dfc333
Wesley Ishida <Wesley.Ishida@scsstudent.org>
2024-09-12 14:51:45 -0700

    Homework 1

---
+++ HW1_Palindrome_Power.py
@@ -0,0 +1,4 @@
+print(111111111*111111111)
+print('Palendromes are cool, but they are cooler in math, so I named this file palindrome power')
+print('In this homework, I also learned that python does not recognize commas in the numbers, so I only got the correct result when the commas in the numbers were removed')
+#To run this in powershell, I would write python HW1_Palindrome_Power.py if I am in the directory, and if i'm not, I would write python (Path name) HW1_Palindrome_Power.py.
